49c7b55d844989014a95e88a0cf658f518bb3a81
fares <matheus.trajano@aiqfome.com>
2023-02-20 23:31:59 -0300

    feat(CCTP-11): database files created for products domain

---
+++ app/Models/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+/**
+ * @property int $code
+ * @property string $status
+ * @property Carbon $imported_t
+ * @property string $url
+ * @property string $creator
+ * @property Carbon $created_t
+ * @property Carbon $last_modified_t
+ * @property string $product_name
+ * @property string $quantity
+ * @property string $brands
+ * @property string $categories
+ * @property string $labels
+ * @property string $cities
+ * @property string $purchase_places
+ * @property string $stores
+ * @property string $ingredients_text
+ * @property string $traces
+ * @property string $serving_size
+ * @property double $serving_quantity
+ * @property int $nutriscore_score
+ * @property string $nutriscore_grade
+ * @property string $main_category
+ * @property string $image_url
+ * */
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+
+    protected $collection = 'products';
+
+
+}
+
