afaa8d9d11b499ff9ce5e47a25b100d41b9b9037
fares <matheus.trajano@aiqfome.com>
2023-02-22 11:59:50 -0300

    fix(CCTP-12): test fix

--- tests/Feature/Commands/ProductsDatabaseUpdateCommandTest.php
+++ tests/Feature/Commands/ProductsDatabaseUpdateCommandTest.php
@@ -2,9 +2,6 @@
 
 use App\Console\Commands\ProductsDatabaseUpdateCommand;
 use App\Services\ProductService;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-
-//uses(RefreshDatabase::class);
 
 it('behaves as expected when the command is called', function () {
     $command = new ProductsDatabaseUpdateCommand(new ProductService());
