54758d97a1b1f590e12f8d530542030b12d9ffd3
fares <matheus.trajano@aiqfome.com>
2023-02-21 22:55:00 -0300

    chore(CCTP-12): cron config at dockerfile

---
+++ app/Console/Commands/ProductsDatabaseUpdate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ProductsDatabaseUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products_database:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'update database of products from open food facts data';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): void
+    {
+
+    }
+}
