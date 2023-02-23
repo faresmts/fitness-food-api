c8806345a63fed33e45227003275c5ff8e595ef0
fares <matheus.trajano@aiqfome.com>
2023-02-22 23:44:55 -0300

    feat(CCTP-18): API Key implemented with response

--- app/Http/Middleware/FitnessFoodsAuth.php
+++ app/Http/Middleware/FitnessFoodsAuth.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use App\Models\ApiKey;
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\HttpFoundation\Response as HttpResponse;
 
 class FitnessFoodsAuth
